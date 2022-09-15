ceabcf2c1ee5f7ab1764a68536c69ad4770d98b8
Luca Destefano B <lucahesper@hotmail.com>
2022-09-15 11:27:05 -0300

    up files

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
-const cardImg = document.querySelector('.modalImg')
+/*let pos, antes = 0, depois = 1, sum = 0, atual = 1, i = 1;
+
+pos = 5;
+
+do {
+    atual = depois + antes;
+    sum += atual;
+
+    antes = depois;
+    depois = atual
+
+    i++
+
+console.log(i);
+
+} while (i < pos);*/
+
 
